7f84801afba56cdd73a2c85525b67dcfd74f7a02
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-11-26 22:04:10 +0800

    completed quick sort visualization

--- src/algorithms/QuickSort.js
+++ src/algorithms/QuickSort.js
@@ -15,35 +15,30 @@ function doQuickSort(arr, startIndex, endIndex, animations) {
 
 function partition(arr, startIndex, endIndex, animations) {
   let pivotIndex = endIndex;
+  animations.push([pivotIndex]);
   // storeIndex is the index of the sorted element we return as partitionIndex in quickSort()
   let storeIndex = startIndex - 1;
 
   for (let pointer = storeIndex + 1; pointer < endIndex; pointer++) {
+    animations.push([pointer, pivotIndex]);
     if (arr[pointer] < arr[pivotIndex]) {
       storeIndex++;
+      animations.push([
+        storeIndex,
+        arr[storeIndex],
+        pointer,
+        arr[pointer],
+        pivotIndex,
+      ]);
       const temp = arr[storeIndex];
       arr[storeIndex] = arr[pointer];
       arr[pointer] = temp;
     }
   }
   storeIndex++;
+  animations.push([storeIndex, arr[storeIndex], pivotIndex, arr[pivotIndex]]);
   const temp = arr[storeIndex];
   arr[storeIndex] = arr[pivotIndex];
   arr[pivotIndex] = temp;
   return storeIndex;
 }
-
-// function quickSort(arr, startIndex, endIndex) {
-//   // recursively call quickSort until left with a single element (which is therefore sorted)
-//   if (startIndex < endIndex) {
-//     // partitionIndex is the index of the sorted element
-//     const partitionIndex = partition(arr, startIndex, endIndex);
-//     quickSort(arr, startIndex, partitionIndex - 1);
-//     quickSort(arr, partitionIndex + 1, endIndex);
-//   }
-//   return arr;
-// }
-
-// const testing = [10, 80, 30, 80, 90, 40, 50, 70];
-// const output = quickSort(testing, 0, testing.length - 1);
-// console.log(output);
