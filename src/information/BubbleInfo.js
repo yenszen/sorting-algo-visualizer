368b78c53d84d42a71bc6178c5a475291f0d8e97
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-11-30 12:37:30 +0800

    added general information on bubble sort

--- src/information/BubbleInfo.js
+++ src/information/BubbleInfo.js
@@ -1,25 +1,39 @@
 import React from "react";
-import { Table } from "reactstrap";
 
 function BubbleInfo() {
   return (
-    <div>
-      <h1>Bubble Sort</h1>
-      <p>
-        The Bubble sort algorithm compares each pair of elements in an array and
-        swaps them if they are out of order until the entire array is sorted.
-        For each element in the list, the algorithm compares every pair of
-        elements.
-      </p>
-      <br></br>
-      <Table dark>
-        <thead>
-          <th>Performance</th>
-        </thead>
-        <tbody>
-          <tr></tr>
-        </tbody>
-      </Table>
+    <div className="info-card">
+      <div className="summary">
+        <h1>Bubble Sort</h1>
+        <p>
+          The Bubble sort algorithm compares each pair of elements in an array
+          and swaps them if they are out of order until the entire array is
+          sorted. For each element in the list, the algorithm compares every
+          pair of elements.
+        </p>
+      </div>
+
+      <br />
+
+      <div className="performance">
+        <h2>Performance</h2>
+        <ul>
+          <li>Worst-case time complexity</li>
+          <li>O(n²)</li>
+        </ul>
+        <ul>
+          <li>Average-case time complexity</li>
+          <li>O(n²)</li>
+        </ul>
+        <ul>
+          <li>Best-case time complexity</li>
+          <li>O(n)</li>
+        </ul>
+        <ul>
+          <li>Worst-case space complexity</li>
+          <li>O(1)</li>
+        </ul>
+      </div>
     </div>
   );
 }
