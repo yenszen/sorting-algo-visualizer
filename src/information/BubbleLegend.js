459c72ef2e36821082b205e0f21b55dfbcfa8dc5
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-11-30 21:16:04 +0800

    added sort legend

--- src/information/BubbleLegend.js
+++ src/information/BubbleLegend.js
@@ -3,6 +3,8 @@ import React from "react";
 function BubbleLegend() {
   return (
     <div className="legend">
+      <div className="block" style={{ backgroundColor: "purple" }}></div>
+      <div style={{ margin: "0 1rem" }}>Sorted</div>
       <div className="block" style={{ backgroundColor: "turquoise" }}></div>
       <div style={{ margin: "0 1rem" }}>Swapping</div>
     </div>
--- src/information/MergeLegend.js
+++ src/information/MergeLegend.js
@@ -3,6 +3,8 @@ import React from "react";
 function MergeLegend() {
   return (
     <div className="legend">
+      <div className="block" style={{ backgroundColor: "purple" }}></div>
+      <div style={{ margin: "0 1rem" }}>Sorted</div>
       <div className="block" style={{ backgroundColor: "gold" }}></div>
       <div style={{ margin: "0 1rem" }}>Comparing</div>
       <div className="block" style={{ backgroundColor: "red" }}></div>
